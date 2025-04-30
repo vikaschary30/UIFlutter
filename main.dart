99c2d278c4ba1794a69fd158970215765c52a0e2
vikaschary30 <vikaschary3001@gmail.com>
2025-04-30 11:11:51 +0530

    Add files via upload
    responsiveui page committed

--- main.dart
+++ main.dart
@@ -1,104 +1,85 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-
 void main() {
   runApp(const MyApp());
 }
-
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+  const MyApp ({super.key});
   @override
-  Widget build(BuildContext context) {
-    MyHomePage homePage=MyHomePage(title: 'Login page');
+  Widget build(BuildContext context){
     return MaterialApp(
-      title: 'SLSF2',
-      color: Colors.white,
-      home: homePage,
-
+      title: 'Responsive UI',
+      home: Responsive(),
     );
   }
 }
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class Responsive extends StatelessWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context){
+    double ScreenW = MediaQuery.of(context).size.width;
+    if(ScreenW<=600){
+      return Mobile();
+    }
+    else if (ScreenW>600 && ScreenW<1200){
+      return Tablet();
+    }
+    else {
+      return Desktop();
+    }
+  }
+}
+class Mobile extends StatefulWidget {
+  const Mobile({super.key});
+  @override
+  State<Mobile> createState() => _Mobile();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-
+class _Mobile extends State<Mobile> {
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+  Widget build(BuildContext m) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('LOGIN PAGE'),
-        backgroundColor: Colors.black45,
-      ),
+      appBar: AppBar(title: Text('Mobile'),),
       body: Center(
-        child: Container(
-
-          color: Colors.black54,
-          width: 500.0,
-          height: 550.0,
-          child: GridView.count(
-              crossAxisCount: 1,
-              children: [
-                Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      Container(
-                        child: Text('LOGIN',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 35.0, decoration: TextDecoration.underline)),
-                      ),
-                      Container(
-                        width: 200.0,
-                        height: 50.0,
-                        child: TextField(
-                          textAlign: TextAlign.center,
-                          decoration: InputDecoration(labelText: 'Enter Username', icon: Icon(Icons.email), border: OutlineInputBorder()),
-                        ),
-                      ),
-                      Container(
-                        width: 200.0,
-                        height: 50.0,
-                        child: TextField(
-                          textAlign: TextAlign.center,
-                          decoration: InputDecoration(labelText: 'Enter Password',icon: Icon(Icons.password), border: OutlineInputBorder()),
-                        ),
-                      ),
-                      Container(
-                        width: 200.0,
-                        height: 50.0,
-                        child: ElevatedButton(onPressed: ()=>{}, child: Text('Submit'))
-                      ),
-                    ],
-                  ),
-                )
-              ],
-          ),
-        ),
+        child: Icon(Icons.mobile_friendly),
       ),
-       // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+class Tablet extends StatefulWidget {
+  const Tablet({super.key});
+  @override
+  State<Tablet> createState() => _Tablet();
+}
+
+class _Tablet extends State<Tablet> {
+  @override
+  Widget build(BuildContext m) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Tablet'),),
+      body: Center(
+        child: Icon(Icons.tablet_android),
+      ),
+    );
+  }
+}
+
+class Desktop extends StatefulWidget {
+  const Desktop({super.key});
+  @override
+  State<Desktop> createState() => _Desktop();
+}
+
+class _Desktop extends State<Desktop> {
+  @override
+  Widget build(BuildContext m) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Desktop'),),
+      body: Center(
+        child: Icon(Icons.desktop_mac),
+      ),
+    );
+  }
+}
+
+
+
+
